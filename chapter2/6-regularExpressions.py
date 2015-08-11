769acbae69ee533d3e1a46c29127f0cfdc2f1d2c
Cynthia O'Donnell <mimibambino@gmail.com>
2015-08-12 00:25:56 +0200

    Revise import statements

--- chapter2/6-regularExpressions.py
+++ chapter2/6-regularExpressions.py
@@ -1,6 +1,6 @@
-from urllib.request 
-import urlopenfrom bs4 
-import BeautifulSoupimport re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
 
 html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html)
