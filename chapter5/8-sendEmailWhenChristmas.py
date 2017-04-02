903b09e04f0c432897ca205961609e53ada7aa7f
yjh <465402634@qq.com>
2017-04-02 23:36:29 +0800

    fix TabError

--- chapter5/8-sendEmailWhenChristmas.py
+++ chapter5/8-sendEmailWhenChristmas.py
@@ -10,9 +10,9 @@ def sendMail(subject, body):
     msg['From'] = "christmas_alerts@pythonscraping.com"
     msg['To'] = "ryan@pythonscraping.com"
 
-	s = smtplib.SMTP('localhost')
-	s.send_message(msg)
-	s.quit()
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
 
 bsObj = BeautifulSoup(urlopen("https://isitchristmas.com/"))
 while(bsObj.find("a", {"id":"answer"}).attrs['title'] == "NO"):
