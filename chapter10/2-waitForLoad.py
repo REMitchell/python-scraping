669e5aeed5845ac628ed6cf56d7a6493e4a5acbf
AaronSolomon <solomon@2017.ipv6.club.tw>
2017-07-31 10:04:49 +0800

    Update 2-waitForLoad.py
    from selenium import webdriver

--- chapter10/2-waitForLoad.py
+++ chapter10/2-waitForLoad.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
