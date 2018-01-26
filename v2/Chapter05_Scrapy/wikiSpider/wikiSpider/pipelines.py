fb9cc07e3e301b74fc873103d81ca2d0ea38be23
remitchell <ryan.e.mitchell@gmail.com>
2018-01-25 23:42:36 -0500

    Added pipelines

--- v2/Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
+++ v2/Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from datetime import datetime
+from wikiSpider.items import Article
+from string import whitespace
 
 class WikispiderPipeline(object):
-    def process_item(self, item, spider):
-        return item
+    def process_item(self, article, spider):
+        dateStr = article['lastUpdated']
+        # This page was last edited on 26 January 2018, at 03:56.
+        article['lastUpdated'] = article['lastUpdated'].replace('This page was last edited on', '')
+        article['lastUpdated'] = article['lastUpdated'].strip()
+        article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M.')
+        article['text'] = [line for line in article['text'] if line not in whitespace]
+        article['text'] = ''.join(article['text'])
+        return article
