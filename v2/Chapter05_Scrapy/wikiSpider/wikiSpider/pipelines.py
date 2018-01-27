235bbe77eed6509e3dedba569b586fe4dde39205
remitchell <ryan.e.mitchell@gmail.com>
2018-01-27 18:19:22 -0500

    Code cleanup

--- v2/Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
+++ v2/Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
@@ -4,8 +4,6 @@ from string import whitespace
 
 class WikispiderPipeline(object):
     def process_item(self, article, spider):
-        dateStr = article['lastUpdated']
-        # This page was last edited on 26 January 2018, at 03:56.
         article['lastUpdated'] = article['lastUpdated'].replace('This page was last edited on', '')
         article['lastUpdated'] = article['lastUpdated'].strip()
         article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M.')
--- v2/Chapter05_Scrapy/wikiSpider/wikiSpider/settings.py
+++ v2/Chapter05_Scrapy/wikiSpider/wikiSpider/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'wikiSpider.pipelines.WikispiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'wikiSpider.pipelines.WikispiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
