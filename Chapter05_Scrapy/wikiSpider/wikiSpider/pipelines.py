22c764876ebec1d7d714a262a116dfcff81b5bde
MustafaGamalMuhammed <45317758+MustafaGamalMuhammed@users.noreply.github.com>
2019-07-21 04:41:54 +0200

    Update pipelines.py

--- Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
+++ Chapter05_Scrapy/wikiSpider/wikiSpider/pipelines.py
@@ -6,7 +6,7 @@ class WikispiderPipeline(object):
     def process_item(self, article, spider):
         article['lastUpdated'] = article['lastUpdated'].replace('This page was last edited on', '')
         article['lastUpdated'] = article['lastUpdated'].strip()
-        article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M.')
+        article['lastUpdated'] = datetime.strptime(article['lastUpdated'], '%d %B %Y, at %H:%M')
         article['text'] = [line for line in article['text'] if line not in whitespace]
         article['text'] = ''.join(article['text'])
         return article
