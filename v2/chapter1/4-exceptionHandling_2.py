09a93651552aee91f9830646f92e46186f8e6171
William <68653853+will-i-amv@users.noreply.github.com>
2021-06-30 10:20:29 -0400

    Add 4th example to chapter1

---
+++ v2/chapter1/4-exceptionHandling_2.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "lxml")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
