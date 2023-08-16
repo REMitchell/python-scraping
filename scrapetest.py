5255b7a8e40d38a962e0bdab95f3fad32ccab18e
masamichiIto <masamichi.ito04@gmail.com>
2023-08-16 23:06:57 +0900

    scrapetest.py updated

--- scrapetest.py
+++ scrapetest.py
@@ -1,4 +1,7 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+#print(html.read())
+bs = BeautifulSoup(html.read(), 'html.parser')
+print(bs.h1)
