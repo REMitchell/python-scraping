b9ae315a2bebf4648ac3ae9271ea20bc6e5a7acd
masamichiIto <masamichi.ito04@gmail.com>
2023-08-21 23:17:20 +0900

    scrapetest2.py, which includes try-excepetion procedure, was created

---
+++ scrapetest2.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+if title == None:
+    print('Title could not be found')
+else:
+    print(title)
