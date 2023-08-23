d86e4b3caf9205dd62ed9733de1db1df24b4a7ce
masamichiIto <masamichi.ito04@gmail.com>
2023-08-23 23:01:57 +0900

    chap3 updated

--- chap3_work.py
+++ chap3_work.py
@@ -8,3 +8,19 @@ for link in bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('
     # ?!から始まる文字列を()で囲むことで，その文字列を含まないを表現できる．(?!:).で1つのコロンを含まない，((?!:).)*コロン以外の0文字以上の文字列を表している
     if 'href' in link.attrs:
         print(link.attrs['href'])
+
+## p.35あたり
+import datetime
+import random
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    return bs.find('div', {'id':'bodyContent'}).find_all('a', href=re.compile('^(/wiki/)((?!:).)*$'))
+
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs['href']
+    print(newArticle)
+    links = getLinks(newArticle)
