9a835fde92507e0fde2968a07f0f5701840358b8
masamichiIto <masamichi.ito04@gmail.com>
2023-10-05 23:59:25 +0900

    finished 3-2-1

--- chap3_work.py
+++ chap3_work.py
@@ -49,3 +49,25 @@ getLinks('')
 """
 
 ## 3-2-1
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    bs = BeautifulSoup(html, 'html.parser')
+    try:
+        print(bs.h1.get_text())
+        print(bs.find(id='mw-content-text').find_all('p')[0])
+        print(bs.find(id='ca-edit').find('span').find('a').attrs['href'])
+    except AttributeError:
+        print('This page is missing something! Continuing.')
+
+    for link in bs.find_all('a', href=re.compile('^(/wiki/)')):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                # 新しいページに出会った
+                newPage = link.attrs['href']
+                print('-'*20)
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks('')
